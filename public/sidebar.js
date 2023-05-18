309d4d637b90230ef9ea77015d30953340649d6f
NitinRana-code <nitinranasd@gmail.com>
2023-05-18 06:36:16 +0530

    changing files

--- public/sidebar.js
+++ public/sidebar.js
@@ -3,12 +3,20 @@ document.addEventListener("DOMContentLoaded", function() {
   
     collapsibleHeaders.forEach(header => {
       header.addEventListener("click", function() {
+        const activeHeaders = document.querySelectorAll(".collapsible-header.active");
+        // activeHeaders.forEach(activeHeader => {
+        //   if (activeHeader !== this) {
+        //     activeHeader.classList.remove("active");
+        //     activeHeader.nextElementSibling.style.display = "none";
+        //   }});
+
         this.classList.toggle("active");
         const content = this.nextElementSibling;
         if (content.style.display === "block") {
           content.style.display = "none";
         } else {
           content.style.display = "block";
+          content.style.marginLeft = "5px";
         }
       });
     });
