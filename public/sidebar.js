4424ad0033f5ec91e967030e76e3892a0631deb7
NitinRana-code <nitinranasd@gmail.com>
2023-05-19 10:20:02 +0530

    updated files

--- public/sidebar.js
+++ public/sidebar.js
@@ -1,24 +1,39 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const collapsibleHeaders = document.querySelectorAll(".collapsible-header");
-  
-    collapsibleHeaders.forEach(header => {
-      header.addEventListener("click", function() {
-        const activeHeaders = document.querySelectorAll(".collapsible-header.active");
-        // activeHeaders.forEach(activeHeader => {
-        //   if (activeHeader !== this) {
-        //     activeHeader.classList.remove("active");
-        //     activeHeader.nextElementSibling.style.display = "none";
-        //   }});
+document.addEventListener("DOMContentLoaded", function () {
+  const collapsibleHeaders = document.querySelectorAll(".collapsible-header");
 
-        this.classList.toggle("active");
-        const content = this.nextElementSibling;
-        if (content.style.display === "block") {
-          content.style.display = "none";
-        } else {
-          content.style.display = "block";
-          content.style.marginLeft = "5px";
-        }
-      });
+  collapsibleHeaders.forEach((header) => {
+    header.addEventListener("click", function () {
+      const activeHeaders = document.querySelectorAll(
+        ".collapsible-header.active"
+      );
+      // activeHeaders.forEach(activeHeader => {
+      //   if (activeHeader !== this) {
+      //     activeHeader.classList.remove("active");
+      //     activeHeader.nextElementSibling.style.display = "none";
+      //   }});
+
+      this.classList.toggle("active");
+      const content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+        content.style.marginLeft = "5px";
+      }
     });
   });
+});
+
+function closeSidebar() {
+  document.getElementById("sidebar").style.width = "0px";
+  document.getElementById("main").style.marginLeft = "0em";
+  document.getElementById("menu").style.display = "block";
+  document.getElementById("menu").style.marginLeft = "3px";
+}
+function openSidebar() {
+  document.getElementById("sidebar").style.width = "280px";
+  document.getElementById("main").style.marginLeft = "18em";
+  document.getElementById("menu").style.display = "none";
   
+
+}
